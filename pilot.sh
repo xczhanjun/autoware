23157efa003f6edc1d2d50704df7b089803b2674
zhanjun <zhanjun@tongxin.cn>
2022-02-01 13:15:59 +0000

    add map samples

--- pilot.sh
+++ pilot.sh
@@ -1,3 +1,5 @@
 docker build -t ghcr.io/xczhanjun/autoware:pilot .
-docker run -it -p 8085:8085 -u 1000 -h witpilot --rm ghcr.io/xczhanjun/autoware:pilot bash
+docker run -it -p 8085:8085 -p 9090:9090 -h witpilot --rm ghcr.io/xczhanjun/autoware:universe bash
+ros2 launch tier4_autoware_launch planning_simulator.launch.xml vehicle_model:=lexus sensor_model:=aip_xx1 map_path:=/home/autoware/map
+#https://github.com/tier4/AutowareArchitectureProposal.proj/tree/main/docker
 
